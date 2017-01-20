576579d2227559e06d2ed9bda36ddbce41eee991
Alkanoor <Alkanoor@github.com>
2017-01-20 14:53:10 +0100

    reinstall

--- attacks/scenario4/auto-reinstall.sh
+++ attacks/scenario4/auto-reinstall.sh
@@ -4,6 +4,8 @@
 if [ $# -ge 1 ]
 then
     echo "Reinstalling app number $1"
+    cd ~/my_apps
+    cd app${i}
     echo onos-app localhost install target/app${i}-1.7.0.oar
     onos-app localhost install target/app${i}-1.7.0.oar
 else
@@ -11,6 +13,8 @@ else
     for i in $(seq 1 8);
     do
         echo "Reinstalling app number ${i}"
+        cd ~/my_apps
+        cd app${i}
         echo onos-app localhost install target/app${i}-1.7.0.oar
         onos-app localhost install target/app${i}-1.7.0.oar
     done
